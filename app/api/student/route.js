33471c6a54c48f50f9d322bc2b69ef50a6de3d6a
Rohit Kumar <rohitrajyadav6522@gmail.com>
2026-09-16 16:49:29 +0530

    Create CRUD Op

--- app/api/student/route.js
+++ app/api/student/route.js
@@ -1,25 +1,62 @@
 import { NextResponse } from "next/server"
 
-export async function GET(){
-  const students = [
-    {id : 1, name:"Rohit",course:"ReactJS"},
-    {id : 2, name:"Ajju",course:"PEA"},
-    {id : 3, name:"OP",course:"NextJS"},
-  ]
-  return NextResponse.json(students)
+const students = [
+  { id: 1, name: "Rohit", course: "ReactJS" },
+  { id: 2, name: "Ajju", course: "PEA" },
+  { id: 3, name: "OP", course: "NextJS" },
+]
+
+export async function GET() {
+  return Response.json(students)
 }
 
 export async function POST(request) {
   const data = await request.json();
+
+  if(Array.isArray(data)){
+    students.push(...data);
+  }else{
+    const newData={
+      id:data.id,
+      name:data.name,
+      course:data.course,
+    }
+    students.push(newData)
+  }
+
   return Response.json({
-    message: "Student Added",
-    students : data
+    message: "Student Added Succesfuly",
+    students: data
+  })
+
+}
+
+
+
+export async function PUT() {
+  const data =  await request.json();
+  const student = students.find(item=>item.id==data.id)
+
+  if(!student){
+    return Response.json({
+      message:"Student not found"
+    })
+  }
+
+  student.name=data.name;
+  student.course=data.course
+  return Response.json({
+    message:"Succesfully",
+    data:student
   })
 }
 
-// export async function PUT() {
-  
-// }
+
+
+
+
+
+
 
 // export async function PATCH(){
 
